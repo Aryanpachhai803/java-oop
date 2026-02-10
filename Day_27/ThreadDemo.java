c8a2824a039a7dc503a097063e96481d8ae178b9
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-10 10:06:40 +0545

    Added delay by try and catch

--- Day_27/ThreadDemo.java
+++ Day_27/ThreadDemo.java
@@ -4,6 +4,12 @@ class mythread implements Runnable{
     public void run(){
         for(int i=0;i<10;i++){
             System.out.println("Hi");
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
         }
     }
 }
