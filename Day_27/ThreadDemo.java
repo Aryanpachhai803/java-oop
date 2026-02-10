47a8b32e924592f82d509f2b68db8163fb6bf17c
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-10 10:17:28 +0545

    Multiple Implementation

--- Day_27/ThreadDemo.java
+++ Day_27/ThreadDemo.java
@@ -13,11 +13,29 @@ class mythread implements Runnable{
         }
     }
 }
+
+class mythread1 implements Runnable{
+    public void run(){
+        for(int i=0;i<10;i++){
+            System.out.println("Hello");
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
 public class ThreadDemo {
     public static void main(String[] args) {
         mythread obj1 = new mythread();
         Thread t = new Thread(obj1);
 
+        mythread1 obj2 = new mythread1();
+        Thread t1 = new Thread(obj2);
+        
         t.start();
+        t1.start();
     }
 }
