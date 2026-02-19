5fbb41685382a555c0c97674019c05b262f907ba
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-19 22:48:57 +0545

    Counter and Increment

--- Day_26/MultiThread.java
+++ Day_26/MultiThread.java
@@ -1,6 +1,15 @@
 package Day_26;
 
 public class MultiThread {
+    class Counter {
+    public int count = 0;
+    public void increment() {
+        count++;
+    }
+    public int getCount() {
+        return count;
+    }
+}
     public static void main(String[] args) {
         
     }
