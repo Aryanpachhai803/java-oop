b22df5ae4d7a09e0b386eddf1e8022b3fa3214e7
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-19 22:52:53 +0545

    Extending Thread

--- Day_26/MultiThread.java
+++ Day_26/MultiThread.java
@@ -2,15 +2,31 @@ package Day_26;
 
 public class MultiThread {
     class Counter {
-    public int count = 0;
-    public void increment() {
-        count++;
+        public int count = 0;
+
+        public void increment() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
     }
-    public int getCount() {
-        return count;
+    class MyThread extends Thread {
+    public Counter counter;
+
+    public MyThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
     }
 }
     public static void main(String[] args) {
-        
+
     }
 }
