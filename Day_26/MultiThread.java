303e2514e7ef7bd61730a9cc568e6945158f7d96
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-19 22:56:38 +0545

    Main

--- Day_26/MultiThread.java
+++ Day_26/MultiThread.java
@@ -1,7 +1,9 @@
 package Day_26;
 
 public class MultiThread {
-    class Counter {
+
+    // Make inner classes static so main can access them
+    static class Counter {
         public int count = 0;
 
         public void increment() {
@@ -12,21 +14,39 @@ public class MultiThread {
             return count;
         }
     }
-    class MyThread extends Thread {
-    public Counter counter;
 
-    public MyThread(Counter counter) {
-        this.counter = counter;
-    }
+    static class MyThread extends Thread {
+        public Counter counter;
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 1000; i++) {
-            counter.increment();
+        public MyThread(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
         }
     }
-}
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();  // create object
+
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        MyThread t3 = new MyThread(counter);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        // Wait for threads to finish
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("Final Counter Value: " + counter.getCount());
     }
 }
