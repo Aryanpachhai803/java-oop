8d4a5f65c2574a8fbfcc7c5019bd1d6f98ad0d44
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-20 22:02:11 +0545

    Color Declartion

--- Day_26/TrafficLight.java
+++ Day_26/TrafficLight.java
@@ -1,7 +1,12 @@
 package Day_26;
 
-public class TrafficLight {
+public class TrafficLight implements Runnable {
+
+    String currentColor;
+
+    public TrafficLight() {
+        currentColor = "RED";
+    }
     public static void main(String[] args) {
-        
     }
 }
