c255fecc757ed7af216d29b35143c494b3618f2b
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-22 23:38:00 +0545

    Constructor

--- Day_26/AtmWithdrawl.java
+++ Day_26/AtmWithdrawl.java
@@ -1,7 +1,17 @@
 package Day_26;
 
 
-public class AtmWithdrawl {
+public class AtmWithdrawl extends Thread {
+    static int accountBalance;
+    String userName;
+    int amountToWithdraw;
+
+    AtmWithdrawl(String userName, int amountToWithdraw) {
+        this.userName = userName;
+        this.amountToWithdraw = amountToWithdraw;
+    }
+
+
     public static void main(String[] args) {
     }
 }
