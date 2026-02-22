9520dbf6367a8f8a8c46d57d065a66ff3313b210
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-22 23:39:55 +0545

    Withdrawl class

--- Day_26/AtmWithdrawl.java
+++ Day_26/AtmWithdrawl.java
@@ -2,6 +2,7 @@ package Day_26;
 
 
 public class AtmWithdrawl extends Thread {
+
     static int accountBalance;
     String userName;
     int amountToWithdraw;
@@ -11,6 +12,30 @@ public class AtmWithdrawl extends Thread {
         this.amountToWithdraw = amountToWithdraw;
     }
 
+    void withdraw() {
+        System.out.println(userName + " is trying to withdraw ₹" + amountToWithdraw);
+
+        if (amountToWithdraw <= accountBalance) {
+
+            try {
+                Thread.sleep(100);  
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            accountBalance -= amountToWithdraw;
+            System.out.println("Withdrawal successful for " + userName);
+            System.out.println("Remaining Balance: ₹" + accountBalance);
+        } else {
+            System.out.println("Insufficient balance for " + userName);
+        }
+
+        System.out.println("----------------------------------");
+    }
+
+    public void run() {
+        withdraw();
+    }
 
     public static void main(String[] args) {
     }
