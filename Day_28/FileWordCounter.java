4834c9f438b66c040eb791591c9712035aa3c0f4
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-23 22:27:38 +0545

    Try Statement

--- Day_28/FileWordCounter.java
+++ Day_28/FileWordCounter.java
@@ -1,6 +1,36 @@
 package Day_28;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class FileWordCounter {
     public static void main(String[] args) {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Enter file name: ");
+            String fileName = consoleReader.readLine();
+            BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            int lineCount = 0;
+            int wordCount = 0;
+            int charCount = 0;
+            while ((line = fileReader.readLine()) != null) {
+                lineCount++;
+                charCount += line.length();
+                if (!line.isEmpty()) {
+                    String[] words = line.split("\\s+");
+                    wordCount += words.length;
+                }
+            }
+            fileReader.close();
+            System.out.println("\nFile Analysis:");
+            System.out.println("Lines: " + lineCount);
+            System.out.println("Words: " + wordCount);
+            System.out.println("Characters: " + charCount);
+
+        }
     }
 }
