1e8f966d8891980ba8dd10df64c24cb0c0634918
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-10 09:55:43 +0545

    Deserializable

---
+++ Day_30/DeserializableDemo.java
@@ -0,0 +1,32 @@
+package Day_30;
+import java.io.*;
+
+class Employee implements Serializable {
+    String name,address;
+    int SSN,number;
+    Employee(String name,String address,int SSN, int number){
+        this.name = name;
+        this.address = address;
+        this.SSN = SSN;
+        this.number = number;
+    }
+}
+
+public class DeserializableDemo{
+    public static void main(String[] args) throws ClassNotFoundException {
+        Employee e = null;
+        try{
+            FileInputStream fis = new FileInputStream("aryanotherdata.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            e = (Employee) ois.readObject();
+        }
+        catch(IOException ie){
+            System.out.println(ie);
+        }
+        System.out.println("Deserialization.....");
+        System.out.println("Name: " + e.name);
+        System.out.println("Address: " + e.address);
+        System.out.println("SSN: " + e.SSN);
+        System.out.println("Number: " + e.number);
+    }
+}
