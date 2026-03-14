75e935a835bfb0a9ec79a68e2f26cee9ce5a45cb
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-14 22:30:43 +0545

    String Compare Initialization

---
+++ Day_31/StringCompare.java
@@ -0,0 +1,11 @@
+package Day_31;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        String s1 = "Nepathya";
+        String s2 = "nepathya";
+
+        
+
+    }
+}
--- Day_31/StringDemo.java
+++ Day_31/StringDemo.java
@@ -5,11 +5,12 @@ public class StringDemo {
         String s1 = "Nepathya College";
         String s2 = "College";
 
-        System.out.println("Compare: " +s1==s2);
         System.out.println("The length is " + s1.length());
         System.out.println("To Lowercase: " + s1.toLowerCase());
         System.out.println("Concate: " + s1.concat(s2));
         System.out.println("CharAt: " + s1.charAt(3));
         System.out.println("Trim: " + s1.trim());
+        System.out.println("Compare: " + s1.compareTo(s2));
+        System.out.println("SubString: " + s1.substring(5));
     }
 }
