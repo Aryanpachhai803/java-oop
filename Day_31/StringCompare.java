a59f92bcdfcb286831c5e83d56326067da515fec
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-14 22:33:20 +0545

    equals

--- Day_31/StringCompare.java
+++ Day_31/StringCompare.java
@@ -5,7 +5,11 @@ public class StringCompare {
         String s1 = "Nepathya";
         String s2 = "nepathya";
 
-        
+        System.out.println("String 1: " + s1);
+        System.out.println("String 2: "+ s2);
+
+        // Checks exact string(Case-sensitive)
+        System.out.println("Equals: " +s1.equals(s2));
 
     }
 }
