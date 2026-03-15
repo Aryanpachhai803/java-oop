8a18c3fbf36966a12222921c7cb86e94576312f3
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-15 20:30:46 +0545

    Deserializable fixed

--- Day_30/DeserializableDemo.java
+++ Day_30/DeserializableDemo.java
@@ -2,9 +2,10 @@ package Day_30;
 import java.io.*;
 
 class Employee implements Serializable {
-    String name,address;
-    int SSN,number;
-    Employee(String name,String address,int SSN, int number){
+    String name, address;
+    int SSN, number;
+
+    Employee(String name, String address, int SSN, int number) {
         this.name = name;
         this.address = address;
         this.SSN = SSN;
@@ -12,22 +13,34 @@ class Employee implements Serializable {
     }
 }
 
-public class DeserializableDemo{
+public class DeserializableDemo {
     public static void main(String[] args) {
+
         Employee e = null;
-        try{
+
+        try {
             FileInputStream fis = new FileInputStream("aryanotherdata.ser");
             ObjectInputStream ois = new ObjectInputStream(fis);
+
             e = (Employee) ois.readObject();
+
+            ois.close();
+            fis.close();
+
+        } catch (IOException ie) {
+            System.out.println("IO Error: " + ie);
+        } catch (ClassNotFoundException ce) {
+            System.out.println("Class not found: " + ce);
         }
-        catch(IOException ie){
-            System.out.println(ie);
+
+        if (e != null) {
+            System.out.println("Deserialization.....");
+            System.out.println("Name: " + e.name);
+            System.out.println("Address: " + e.address);
+            System.out.println("SSN: " + e.SSN);
+            System.out.println("Number: " + e.number);
+        } else {
+            System.out.println("Employee object could not be deserialized.");
         }
-        System.out.println("Deserialization.....");
-        System.out.println("Name: " + e.name);
-        System.out.println("Address: " + e.address);
-        System.out.println("SSN: " + e.SSN);
-        System.out.println("Number: " + e.number);
     }
 }
- 
