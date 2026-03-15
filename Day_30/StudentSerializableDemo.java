4907243a5fcafffa83a3b3b76d01f5a00394766d
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-15 20:36:22 +0545

    Student Constructor

--- Day_30/StudentSerializableDemo.java
+++ Day_30/StudentSerializableDemo.java
@@ -1,4 +1,17 @@
 package Day_30;
+import java.io.*;
+
+class Student implements Serializable {
+    String name, address;
+    int id, marks;
+
+    Student(String name, String address, int id, int marks){
+        this.name = name;
+        this.address = address;
+        this.id = id;
+        this.marks = marks;
+    }
+}
 
 public class StudentSerializableDemo {
     public static void main(String[] args) {
