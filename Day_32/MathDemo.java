45fbec75c40ab06bec68eb40f1feb2c8430f5106
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-16 08:30:37 +0545

    Math maximum comparison

---
+++ Day_32/MathDemo.java
@@ -0,0 +1,11 @@
+package Day_32;
+import java.lang.Math;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 30;
+
+        System.out.println("Max is: " + Math.max(x,y));
+    }
+}
