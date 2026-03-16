dfd636f9b7ede1e55eada53da28c447051361a4f
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-16 08:54:57 +0545

    Converting into Object Oriented

---
+++ Day_32/WrapperDemo.java
@@ -0,0 +1,17 @@
+package Day_32;
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        int i = 10;
+        Integer i1 = new Integer(i); //Boxing 
+        int j = Integer.valueOf(i1); //Unboxing
+        Integer v = i;               //Auto-Boxing
+        int w = v;                   //Auto-Unboxing
+
+        System.out.println(i);
+        System.out.println(i1);
+        System.out.println(j);
+        System.out.println(v);
+        System.out.println(w);
+    }
+}
