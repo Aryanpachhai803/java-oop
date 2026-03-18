e33afe609222e76d97ed1a098eee5e2a69b5c400
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-18 21:29:48 +0545

    Initialization of a text

---
+++ Day_32/StringWrapper.java
@@ -0,0 +1,11 @@
+package Day_32;
+
+public class StringWrapper {
+    String text;
+    public StringWrapper(String text){
+        this.text = text;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
