d9e23865bf2c65c681f916469cb0e6c5934fb919
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-18 21:34:54 +0545

    Checking vowels

--- Day_32/StringWrapper.java
+++ Day_32/StringWrapper.java
@@ -5,6 +5,19 @@ public class StringWrapper {
     public StringWrapper(String text){
         this.text = text;
     }
+
+    public String getVowels(){
+        String result = "";
+
+        for(int i=0;i<text.length();i++){
+            char c = text.charAt(i);
+
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+                result +=c;
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
         
     }
