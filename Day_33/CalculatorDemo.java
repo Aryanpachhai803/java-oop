771bb20b17ebb31a2eda46b819a214cf23873929
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-19 21:57:34 +0545

    Initialization of button and operand, operators

--- Day_33/CalculatorDemo.java
+++ Day_33/CalculatorDemo.java
@@ -1,6 +1,43 @@
 package Day_33;
 import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
 
-public class CalculatorDemo extends Applet  {
+public class CalculatorDemo extends Applet implements ActionListener {
 
-}
+    TextField t1, t2, result;
+    Button add, sub, mul, div;
+
+    public void init() {
+        setLayout(new GridLayout(5, 2));
+        t1 = new TextField();
+        t2 = new TextField();
+        result = new TextField();
+        result.setEditable(false);
+
+        add = new Button("+");
+        sub = new Button("-");
+        mul = new Button("*");
+        div = new Button("/");
+
+        add(new Label("Enter first number:"));
+        add(t1);
+
+        add(new Label("Enter second number:"));
+        add(t2);
+
+        add(add);
+        add(sub);
+        add(mul);
+        add(div);
+
+        add(new Label("Result:"));
+        add(result);
+
+        add.addActionListener(this);
+        sub.addActionListener(this);
+        mul.addActionListener(this);
+        div.addActionListener(this);
+    }
+
+    }
