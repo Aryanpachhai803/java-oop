21167359aa82d92f072f7b8ca9ae10cbd5c4773b
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-20 19:19:17 +0545

    Initialization of Login form

--- Day_29/PasswordChecker.java
+++ Day_29/PasswordChecker.java
@@ -1,7 +1,16 @@
 package Day_29;
+import java.util.*;
+import java.io.*;
 
 public class PasswordChecker {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String inputUser = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String inputPass = sc.nextLine();
+
     }
 }
