0807442fe521d2e1cdfa0cdc23477fd8ea466860
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-21 23:58:11 +0545

    Main method

--- Day_14/Bank.java
+++ Day_14/Bank.java
@@ -1,4 +1,5 @@
 package Day_14;
+import java.util.Scanner;
 
 abstract class BankAccount {
     String accountHolder;
@@ -32,7 +33,6 @@ abstract class BankAccount {
 }
 
 class SavingsAccount extends BankAccount {
-
     SavingsAccount(String name, double balance) {
         super(name, balance);
     }
@@ -44,7 +44,6 @@ class SavingsAccount extends BankAccount {
 }
 
 class CurrentAccount extends BankAccount {
-
     CurrentAccount(String name, double balance) {
         super(name, balance);
     }
@@ -57,5 +56,68 @@ class CurrentAccount extends BankAccount {
 public class Bank {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter account holder name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter initial balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.println("Choose Account Type: 1. Savings  2. Current");
+        int choice = sc.nextInt();
+
+        BankAccount account;
+
+        if (choice == 1) {
+            account = new SavingsAccount(name, balance);
+        } else {
+            account = new CurrentAccount(name, balance);
+        }
+
+        int option;
+
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Calculate Interest");
+            System.out.println("5. Exit");
+            System.out.print("Choose option: ");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("Enter amount: ");
+                    double dep = sc.nextDouble();
+                    account.deposit(dep);
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount: ");
+                    double wit = sc.nextDouble();
+                    account.withdraw(wit);
+                    break;
+
+                case 3:
+                    account.displayBalance();
+                    break;
+
+                case 4:
+                    account.calculateInterest();
+                    break;
+
+                case 5:
+                    System.out.println("Thank you!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+        } while (option != 5);
+
+        sc.close();
     }
 }
