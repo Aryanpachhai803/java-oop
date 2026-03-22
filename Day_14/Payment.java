4fd74846a42e88c1d00838080473c6def10e9b53
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-22 23:11:58 +0545

    Creditcard implementation

--- Day_14/Payment.java
+++ Day_14/Payment.java
@@ -4,6 +4,13 @@ interface Pay {
     void pay(double amount);
 }
 
+class CreditCardPayment implements Pay {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card.");
+    }
+}
+
+
 public class Payment {
     public static void main(String[] args) {
 
