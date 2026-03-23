fb179abd8890a7daa6901d5bfe79530ec8ccc2bd
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-23 23:18:03 +0545

    Choice

--- Day_14/FoodOrder.java
+++ Day_14/FoodOrder.java
@@ -28,7 +28,85 @@ public class FoodOrder {
         }
     }
 
-    public static void main(String[] args) {
-        
+    static class Burger extends Food {
+        Burger() {
+            super("Burger");
+        }
+
+        @Override
+        double getPrice() {
+            return 250;
+        }
     }
+
+    static class Sandwich extends Food {
+        Sandwich() {
+            super("Sandwich");
+        }
+
+        @Override
+        double getPrice() {
+            return 150;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Food> orders = new ArrayList<>();
+        ArrayList<Integer> quantities = new ArrayList<>();
+
+        int choice = 0;
+
+        do {
+            System.out.println("\n=== Food Menu ===");
+            System.out.println("1. Pizza (Rs.500)");
+            System.out.println("2. Burger (Rs.250)");
+            System.out.println("3. Sandwich (Rs.150)");
+            System.out.println("4. Checkout");
+            System.out.print("Enter your choice: ");
+
+            if (!sc.hasNextInt()) {
+                System.out.println("Please enter a valid number!");
+                sc.next(); 
+                continue;
+            }
+
+            choice = sc.nextInt();
+            Food food = null;
+
+            switch(choice) {
+                case 1:
+                    food = new Pizza();
+                    break;
+                case 2:
+                    food = new Burger();
+                    break;
+                case 3:
+                    food = new Sandwich();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    continue;
+            }
+
+            if (choice >= 1 && choice <= 3) {
+                System.out.print("Enter quantity: ");
+
+                if (!sc.hasNextInt()) {
+                    System.out.println("Invalid quantity!");
+                    sc.next();
+                    continue;
+                }
+
+                int qty = sc.nextInt();
+
+                orders.add(food);
+                quantities.add(qty);
+
+                System.out.println(food.name + " added to cart!");
+            }
+
+        }    }
 }
