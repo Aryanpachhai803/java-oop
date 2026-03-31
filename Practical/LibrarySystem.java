2316c4da9895605ebe9c1fde562d04f466615579
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-31 22:23:16 +0545

    Initialization of Library System

---
+++ Practical/LibrarySystem.java
@@ -0,0 +1,10 @@
+package Practical;
+
+public class LibrarySystem {
+    static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    static final String USER = "root";
+    static final String PASSWORD = "your_password";
+    public static void main(String[] args) {
+        
+    }
+}
