d2a250b3f85702a9967b248dc80e84cfc410f183
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-31 22:24:08 +0545

    Connection

--- Practical/LibrarySystem.java
+++ Practical/LibrarySystem.java
@@ -1,9 +1,21 @@
 package Practical;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class LibrarySystem {
     static final String URL = "jdbc:mysql://localhost:3306/library_db";
     static final String USER = "root";
     static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Database connection failed!");
+            return null;
+        }
+    }
     public static void main(String[] args) {
         
     }
