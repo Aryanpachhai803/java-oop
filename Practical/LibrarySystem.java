f6ab461b77fad222767623c16f13061199bc0259
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-31 22:24:44 +0545

    Adding Book

--- Practical/LibrarySystem.java
+++ Practical/LibrarySystem.java
@@ -2,6 +2,7 @@ package Practical;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class LibrarySystem {
     static final String URL = "jdbc:mysql://localhost:3306/library_db";
@@ -14,6 +15,22 @@ public class LibrarySystem {
         } catch (Exception e) {
             System.out.println("Database connection failed!");
             return null;
+        }
+    }
+
+    public static void addBook(String title, String author) {
+        try (Connection con = getConnection()) {
+            String query = "INSERT INTO books(title, author, available) VALUES (?, ?, true)";
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1, title);
+            ps.setString(2, author);
+
+            ps.executeUpdate();
+            System.out.println("✅ Book added!");
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
     public static void main(String[] args) {
